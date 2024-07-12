01d8ce1e30c6af2da95348091d9f8418648ad5d4
Martin Engqvist <martin@enginzyme.com>
2024-07-12 22:01:15 +0200

    add config to make it easier to locate data

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+# Define the paths
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, '..'))
+DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
